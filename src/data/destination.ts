84145d4e1d64b842703947584f3300dff3805898
Your Name <your.email@example.com>
2025-07-08 15:39:03 +0300

    completed handling the destinations page.

--- src/data/destination.ts
+++ src/data/destination.ts
@@ -4,7 +4,7 @@ export const destinations = [
     description: "Explore the stunning peaks and diverse wildlife of Mt. Kenya.",
     groupPrice: "$1200",
     individualPrice: "$1500",
-    image: "/images/featured/mt-kenya.jpeg"
+    image: "/images/featured/Mt.Kenya.jpeg"
   },
   {
     name: "Mt. Ruwenzori",
@@ -18,27 +18,27 @@ export const destinations = [
     description: "Discover the flamingos and rich birdlife at Lake Nakuru.",
     groupPrice: "$900",
     individualPrice: "$1100",
-    image: "/images/lake-nakuru.jpg"
+    image: "/images/featured/L.Nakuru.jpeg"
   },
   {
     name: "Tsavo National Park",
     description: "Safari through the vast landscapes of Tsavo National Park.",
     groupPrice: "$1000",
     individualPrice: "$1200",
-    image: "/images/tsavo.jpg"
+    image: "/images/featured/Tsavo.jpeg"
   },
   {
     name: "Nairobi National Park",
     description: "Enjoy wildlife viewing just outside the city at Nairobi National Park.",
     groupPrice: "$800",
     individualPrice: "$1000",
-    image: "/images/nairobi-national-park.jpg"
+    image: "/images/featured/nnp.jpeg"
   },
   {
     name: "Coastal Kenya",
     description: "Relax on the beautiful beaches and explore coastal culture.",
     groupPrice: "$1100",
     individualPrice: "$1300",
-    image: "/images/coastal-kenya.jpg"
+    image: "/images/featured/Malindi.jpeg"
   }
 ]
--- src/pages/destinations.tsx
+++ src/pages/destinations.tsx
@@ -4,8 +4,8 @@ import { destinations } from '../data/destination';
 
 export const Destinations: React.FC = () => {
     return (
-        <Container>
-            <Grid container spacing={4}>
+        <Container sx={{ mb: 6}}>
+            <Grid container spacing={3}>
                 {destinations
                     .filter(
                         (dest) =>
@@ -16,9 +16,16 @@ export const Destinations: React.FC = () => {
                             typeof dest.image === 'string'
                     )
                     .map((dest) => (
-                        <Box key={dest.name} sx={{ mb: 6, display: 'flex', flexWrap: 'wrap', gap: 4 }}>
-                            <Box>
-                                <DestinationCard {...dest} />
+                        <Box key={dest.name}> 
+                            <Box  sx={{
+                                height: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                maxWidth:"350px",
+                                margin:"10px 5px"
+                            }}>
+                                <DestinationCard {...dest} 
+                                />
                             </Box>
                         </Box>
                     ))}
